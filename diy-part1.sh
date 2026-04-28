e1b2ba63ad8cab9c276310fe8a848762ff0c9753
yueyu5 <zhq_234@163.com>
2026-04-28 22:45:40 +0800

    Clean up configuration settings in diy-part1.sh
    Removed unnecessary configuration lines for dnsmasq and IPv6 support.

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,13 +16,3 @@ sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt76
 
 # 添加SSR-Plus官方稳定源
 echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
-
-# SSR-Plus必需依赖：替换dnsmasq为full版本
-sed -i 's/CONFIG_PACKAGE_dnsmasq=y/CONFIG_PACKAGE_dnsmasq=n/g' .config 2>/dev/null
-echo "CONFIG_PACKAGE_dnsmasq-full=y" >> .config
-
-# 32M适配：根分区大小设置为28M，和固件上限匹配
-#echo "CONFIG_TARGET_ROOTFS_PARTSIZE=280" >> .config
-
-# 开启IPv6支持
-echo "CONFIG_PACKAGE_ipv6helper=y" >> .config
