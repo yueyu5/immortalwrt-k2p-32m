8cae6d90fd1f4308490a02a0c9d08bfafcddbd93
yueyu5 <zhq_234@163.com>
2026-04-28 09:37:17 +0800

    Comment out broken-flash-reset in mt7621_phicomm_k2p.dts
    Comment out the broken-flash-reset line in the K2P device tree.

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@ sed -i 's/15744k/31744k/g' target/linux/ramips/image/mt7621.mk
 sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 #降低SPI时钟
 sed -i 's/<50000000>/<10000000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+#sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 
 
