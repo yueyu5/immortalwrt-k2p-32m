156dab7e7c7f72ec7b62fc3fc6582568cf8963a5
yueyu5 <zhq_234@163.com>
2026-04-29 18:58:21 +0800

    Remove comment on SPI clock reduction
    Removed comment about lowering SPI clock.

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,6 @@
 ###### K2P-32M修改编译文件 ######
 sed -i 's/15744k/31744k/g' target/linux/ramips/image/mt7621.mk
 sed -i 's/"Phicomm K2P";/"Phicomm K2P-32M";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-#降低SPI时钟
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 
