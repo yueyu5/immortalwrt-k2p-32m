3fb0e600a2a7b074e6d032915099eac67e7c0764
yueyu5 <zhq_234@163.com>
2026-04-29 15:14:23 +0800

    Add personalization configurations to diy-part2.sh
    This script customizes the configuration for a build, including timezone, language, SSIDs, software sources, IP address, and hostname.

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,8 +2,29 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #  feeds更新后执行，仅做个性化配置，不修改核心启动参数
 
+echo "开始个性化配置..."
+
+# 修改时区（默认 UTC，改为中国标准时间）
+sed -i "s/timezone='UTC'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
+sed -i "/timezone='CST-8'/a\\\t\tset system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+
+# 修改默认语言为中文
+sed -i "s/option lang 'en'/option lang 'zh-cn'/g" package/base-files/files/bin/config_generate
+
+# 修改 2.4G 默认 SSID
+sed -i 's/ImmortalWrt/opo-2.4G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改 5G 默认 SSID
+sed -i 's/ImmortalWrt_5G/opo-5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 替换默认软件源为清华源
+sed -i 's/downloads.immortalwrt.org/mirrors.tuna.tsinghua.edu.cn\/immortalwrt/g' package/emortal/emortal-files/etc/opkg/distfeeds.conf
+sed -i 's/immortalwrt.pro/mirrors.tuna.tsinghua.edu.cn\/immortalwrt/g' package/emortal/emortal-files/etc/opkg/distfeeds.conf
+
 # 修改默认IP（可选，默认192.168.1.1，按需修改）
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主机名
 sed -i 's/ImmortalWrt/K2P-32M/g' package/base-files/files/bin/config_generate
+
+echo "个性化配置完成！"
